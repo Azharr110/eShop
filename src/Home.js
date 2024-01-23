029f6b82205ac0f29df1951b5013e635c6faa2ac
Azharr110 <abdazhar786@gmail.com>
2024-01-23 22:50:03 +0530

    Add more items

--- src/Home.js
+++ src/Home.js
@@ -14,19 +14,20 @@ function Home() {
 
         <div className="home__row">
             <Product
-                id="12321341"
-                title="Bennett Mystic 15.6 inch Laptop Shoulder Messenger Sling Office Bag, Water Repellent Fabric for Men and Women (Blue)"
-                price={11.96}
-                rating={5}
-                image="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg"
-            />
-            <Product
                 id="49538094"
                 title="IFB 30 L Convection Microwave Oven (30BRC2, Black, With Starter Kit)"
                 price={239.0}
                 rating={4}
                 image="https://images-na.ssl-images-amazon.com/images/I/81D8pNFmWzL._SL1500_.jpg"
             />
+            <Product
+                id="12321341"
+                title="Bennett Mystic 15.6 inch Laptop Shoulder Messenger Sling Office Bag, Water Repellent Fabric for Men and Women (Blue)"
+                price={11.96}
+                rating={5}
+                image="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg"
+            />
+            
         </div>
 
         <div className="home__row">
@@ -38,18 +39,18 @@ function Home() {
                 image="https://images-eu.ssl-images-amazon.com/images/I/514kNkerQ0L._SY264_BO1,204,203,200_QL40_FMwebp_.jpg"
             />
             <Product
-                id="23445930"
-                title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
-                price={98.99}
-                rating={5}
-                image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
-            />
-            <Product
                 id="3254354345"
                 title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
                 price={598.99}
                 rating={4}
                 image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
+            />
+              <Product
+                id="23445930"
+                title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
+                price={98.99}
+                rating={5}
+                image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
             />
         </div>
 
