c28103f9d42722c7b9055e271451e9ab56ad3bac
Azharr110 <abdazhar786@gmail.com>
2024-10-08 17:18:51 +0530

    update login

--- src/Login.js
+++ src/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Login.css'
 import { Link, useNavigate } from "react-router-dom";
 import StorefrontIcon from '@mui/icons-material/Storefront';
@@ -9,13 +9,20 @@ function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+  
+    useEffect(() => {
+        setEmail('');   
+        setPassword(''); 
+    }, []); 
+
     const signIn = e => {
         e.preventDefault();
 
         auth
             .signInWithEmailAndPassword(email, password)
             .then(auth => {
-                navigate('/');
+                navigate('/home');
+            
             })
             .catch(error => alert(error.message))
 
@@ -28,7 +35,8 @@ function Login() {
             .createUserWithEmailAndPassword(email, password)
             .then((auth) => {
                 if (auth) {
-                    navigate('/');
+                    navigate('/home');
+                   
                 }
             })
             .catch(error => alert(error.message))
@@ -49,10 +57,12 @@ function Login() {
 
                 <form>
                     <h5>E-mail</h5>
-                    <input type='text' value={email} onChange={e => setEmail(e.target.value)} />
+                    <input type='text' value={email} onChange={e => setEmail(e.target.value)}   name={`email-${Math.random()}`} // Random name
+                        autoComplete="new-email"   />
 
                     <h5>Password</h5>
-                    <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
+                    <input type='password' value={password} onChange={e => setPassword(e.target.value)} name={`password-${Math.random()}`} // Random name
+                        autoComplete="new-password"  />
 
                     <button type='submit' className='login__signInButton' onClick={signIn}>Sign In</button>
                 </form>
